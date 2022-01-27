3eddd96a150b7d5eb16d8016c07189d5f7f9ef25
shivammishra1 <43274506+shivammishra1@users.noreply.github.com>
2022-01-27 10:12:51 +0530

    commit1

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+//Camel convention(for classnames):All word's first letter must be in uppercase.
+class FirstProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+
+	//pascal convention(for methods):first word's first letter should be in lowercase and
+	//suceeding word's first letter should be in upper case.
+	public static void secondMethodJava()
+	{
+		System.out.println("Hello World!");
+	}
+}
